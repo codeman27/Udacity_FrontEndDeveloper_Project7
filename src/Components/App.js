52de5b8f1fea012a069670e13f5597526420e3eb
codeman27 <codeman2727@gmail.com>
2018-09-17 21:30:16 -0600

    decided to use Material-UI

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
-import '../CSS/App.css'
 import Map from './Map.js'
+import '../CSS/App.css'
 
 class App extends Component {
+  state = {
+    places: [{title: 'Rockslide Brewery', name: 'Rockslide', position: {lat: 39.0670, lng: -108.5660}},
+      {title: 'Edgewater Brewery', name: 'Edgewater', position: {lat: 39.0553, lng: -108.5578}},
+      {title: 'Palisade Brewing Co.', name: 'Palisade', position: {lat: 39.1111, lng: -108.3543}},
+      {title: 'Kannah Creek Brewing Co.', name: 'Kannah', position: {lat: 39.0853, lng: -108.5520}},
+      {title: 'Copper Club Brewing Co.', name: 'Copper', position: {lat: 39.1591, lng: -108.7315}},
+    ],
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <Map/>
+        <Map places={this.state.places}/>
       </div>
     )
   }
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -2,14 +2,6 @@ import React, { Component } from 'react'
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
-  state = {
-    places: [{title: 'Rockslide Brewery', name: 'Rockslide', position: {lat: 39.0670, lng: -108.5660}},
-      {title: 'Edgewater Brewery', name: 'Edgewater', position: {lat: 39.0553, lng: -108.5578}},
-      {title: 'Palisade Brewing Co.', name: 'Palisade', position: {lat: 39.1111, lng: -108.3543}},
-      {title: 'Kannah Creek Brewing Co.', name: 'Kannah', position: {lat: 39.0853, lng: -108.5520}},
-      {title: 'Copper Club Brewing Co.', name: 'Copper', position: {lat: 39.1591, lng: -108.7315}},
-      ]
-  }
   render() {
     return (
       <Map
@@ -18,7 +10,7 @@ export class MapContainer extends Component {
         initialCenter={{lat: 39.0639,
           lng: -108.5506}}>
 
-        {this.state.places.map((place) => {
+        {this.props.places.map((place) => {
           return (
             <Marker
               key={place.name}
